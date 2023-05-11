afc771c7d9c21347b2b43b5bfdd3a5187863b5ff
d4v1dwalstadh <113419628+d4v1dwalstadh@users.noreply.github.com>
2023-05-11 14:51:33 +0200

    Delete russekort.database.html directory

--- russekort.database.html/database.js
+++
@@ -1,8 +0,0 @@
-let russekortOptions = {
-    "personer": [
-      { "navn": "Peter Stordalen", "skole": "Røyken Videregående Skole", "adresse": "Røyken", "tlf": "samsung", "quote": "Make money get friends", "farge":"blaa", "bilde": "https://premium.vgc.no/v2/images/6fe2247f-c0c5-4307-a8ac-281c4b97dab2?fit=crop&format=auto&h=1536&w=2048&s=19b4b8e95461e0c55d608713bfcd8e5d1fbbe11f" },
-      { "navn": "Eskil Madsen", "skole": "Drøbak Videregående Skole", "adresse": "Halden", "tlf": "Iphone 14 pro max 528gb", "quote": "Yolo, russetid", "farge":"rod", "bilde": "https://media.snl.no/media/236853/standard_eskil-pedersen.jpg" },
-      { "navn": "Mons", "skole": "Lillehammer Folkehøyskole", "adresse": "Skolebenken", "tlf": "410 12 342", "quote": "Lev livet, ha det høy", "farge":"blaa", "bilde": "https://www.dyreklinikk.no/media/global/dyreklinikk_no/mattilsynet/katt/3/mt-katt-forside_4x4.jpg?cdv=636420270510000000&width=950" },
-      { "navn": "Jens Fritz", "skole": "Oslo Handels Gymnasium", "adresse": "Grønland", "tlf": "ja", "quote": "Hei og hå", "farge":"rod", "bilde": "https://i1.sndcdn.com/artworks-qxE1uUEV4TAb8xrt-QwxmlQ-t240x240.jpg" }
-    ]
-  }
